c4611268d09ce3fcd98fc645e05e534106b8ea26
Sergio Moreno <sergiomoreno@realtracksystems.com>
2022-02-03 23:16:46 +0100

    fixing list style

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -19,7 +19,7 @@ const ProductItem = ({product}) => {
 	}
 	return (
 		<div className={styles.ProductItem}>
-			<Image src={product.images[0]} alt={product.title} width="100%" height="100%"/>
+			<Image src={product.images[0]} alt={product.title} width="240px" height="240px"/>
 			<div className={styles['product-info']}>
 				<div>
 					<p>${product.price}</p>
