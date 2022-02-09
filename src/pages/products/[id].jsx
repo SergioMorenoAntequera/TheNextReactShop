5d5836bf34ff6cd5872abebccf15583275044a63
Sergio Moreno <sergiomoreno@realtracksystems.com>
2022-02-09 21:51:31 +0100

    getting unique product by id

---
+++ src/pages/products/[id].jsx
@@ -0,0 +1,22 @@
+import useGetApiData from '@hooks/useGetApiData'
+import React from 'react'
+
+
+function Product({product}) {
+
+    console.log(product)
+    return (
+        <div>
+            {product.title}
+        </div>
+    )
+}
+
+const API = "https://api.escuelajs.co/api/v1/products";
+Product.getInitialProps = async ({query}) => {
+    let request = await fetch(`${API}/${query.id}`)
+    let product = await request.json();
+    return { product: product }
+}
+
+export default Product;
